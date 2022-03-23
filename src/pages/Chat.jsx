db9979d983b360522e4f7122f1e7b94c87cf7234
dinel <solahuddin.hafid.tomaballo@gmail.com>
2022-03-23 19:27:14 +0800

    try to append

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -9,7 +9,6 @@ import { showNotif } from "../store/notifSlice";
 import { useLocation } from "react-router";
 
 const Friend = ({ friend, setPeople, people }) => {
-  console.log(friend, people);
   return (
     <div
       className={
@@ -26,6 +25,7 @@ const Friend = ({ friend, setPeople, people }) => {
           {friend.last_message}
         </p>
       </div>
+      {friend.notif &&<p>notif</p>}
     </div>
   );
 };
@@ -63,6 +63,93 @@ export default function Chat({ socket }) {
   const dispatch = useDispatch();
   const { state } = useLocation();
 
+  const getReadChat = useCallback(
+    async (friendId) => {
+      try {
+        setLdRead(true);
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/chat/reads/${userId}/${friendId}`
+        );
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        data.messages?.length > 0 &&
+          setMessages((prev) => [...prev, ...data.messages]);
+      } catch (error) {
+        console.log(error);
+        dispatch(
+          showNotif({
+            type: "Error",
+            message: "Gagal mengambil data pesan lama",
+            action: null,
+          })
+        );
+      } finally {
+        setLdRead(false);
+      }
+    },
+    [dispatch, userId]
+  );
+
+  const getUnreadChat = useCallback(
+    async (friendId) => {
+      try {
+        setLdUnrd(true);
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/chat/unreads/${userId}/${friendId}`
+        );
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        data.messages?.length > 0
+          ? setMessages((prev) => [...prev, ...data.messages])
+          : getReadChat(friendId);
+      } catch (error) {
+        console.log(error);
+        dispatch(
+          showNotif({
+            type: "Error",
+            message: "Gagal mengambil data teman",
+            action: null,
+          })
+        );
+      } finally {
+        setLdUnrd(false);
+      }
+    },
+    [dispatch, getReadChat, userId]
+  );
+
+  // make chat read to assign the message as readed
+  const makeChatRead = useCallback(
+    async (friendId) => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/chat/read/`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              user: parseInt(userId),
+              friend: parseInt(friendId),
+            }),
+          }
+        );
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [userId]
+  );
+
   useEffect(() => {
     if (state) {
       setPeople({ friend: state.user.id, name: state.user.name });
@@ -74,9 +161,28 @@ export default function Chat({ socket }) {
     const data = JSON.parse(e.data);
     if (data.type !== "Notif") {
       setMessages((prev) => [...prev, data]);
+      // jika pengirim pesan sama dengan people yang sedang dilihat
+      // maka make chat read
+      // jika tidak maka tampahkan notif ke frinds jika ada
+      if (data.from === people?.friend) {
+        makeChatRead(data.from);
+      } else {
+        let finded = false;
+        for (let i = 0; i < friends.length; i++) {
+          if (friends[i].friend === data.from) {
+            console.log("ada");
+            friends[i].notif = true;
+            setFriends([...friends]);
+            finded = true;
+            break;
+          }
+        }
+        if (!finded) {
+          setFriends([{friend: data.from, notif: true, last_message: data.message}, ...friends]);
+        }
+      }
     }
   };
-
   useEffect(() => {
     const getAllFriends = async () => {
       try {
@@ -88,7 +194,8 @@ export default function Chat({ socket }) {
         if (!response.ok) {
           throw new Error(data.message);
         }
-        data.friends?.length > 0 && setFriends((prev) => [...prev, ...data.friends]);
+        data.friends?.length > 0 &&
+          setFriends((prev) => [...prev, ...data.friends]);
       } catch (error) {
         console.log(error);
         dispatch(
@@ -105,67 +212,28 @@ export default function Chat({ socket }) {
     userId && getAllFriends();
   }, [userId, dispatch]);
 
-  const getUnreadChat = useCallback(
-    async (friendId) => {
-      try {
-        setLdUnrd(true);
-        const response = await fetch(
-          `${process.env.REACT_APP_SERVER_URL}/chat/unreads/${userId}/${friendId}`
-        );
-        const data = await response.json();
-        if (!response.ok) {
-          throw new Error(data.message);
-        }
-          data.messages?.length > 0 &&
-          setMessages((prev) => [...prev, ...data.messages]);
-      } catch (error) {
-        console.log(error);
-        dispatch(
-          showNotif({
-            type: "Error",
-            message: "Gagal mengambil data teman",
-            action: null,
-          })
-        );
-      } finally {
-        setLdUnrd(false);
-      }
-    },
-    [dispatch, userId]
-  );
-  const getReadChat = useCallback(
-    async (friendId) => {
-      try {
-        setLdRead(true);
-        const response = await fetch(
-          `${process.env.REACT_APP_SERVER_URL}/chat/reads/${userId}/${friendId}`
-        );
-        const data = await response.json();
-        if (!response.ok) {
-          throw new Error(data.message);
-        }
-        setMessages((prev) => [...prev, ...data.messages]);
-      } catch (error) {
-        console.log(error);
-        dispatch(
-          showNotif({
-            type: "Error",
-            message: "Gagal mengambil data teman",
-            action: null,
-          })
-        );
-      } finally {
-        setLdRead(false);
-      }
-    },
-    [dispatch, userId]
-  );
-
   useEffect(() => {
     if (people && people.friend) {
       getUnreadChat(people.friend);
     }
   }, [people, getUnreadChat]);
+
+  // useEffect(() => {
+  //   if (messages?.length > 0) {
+  //     let friendID;
+  //     // untuk mengambil id friend dari pesan yang masuk
+  //     for (let i = 0; i < messages.length; i++) {
+  //       if (messages[i].from !== parseInt(userId)) {
+  //         friendID = messages[i].from;
+  //         break;
+  //       }
+  //     }
+  //     // jika id frined pengirim pesan yang masuk tidak sama dengan id people yang tampil
+  //     // maka kirimkan chat telah dibaca
+  //     // jika tidak maka
+  //     friendID === people?.friend && makeChatRead(messages[0].from);
+  //   }
+  // }, [messages, makeChatRead, people, userId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -228,14 +296,19 @@ export default function Chat({ socket }) {
         >
           <ul className="space-y-2">
             {!ldUnrd ? (
-              messages && messages.length > 0 ? (
-                messages.map((item, index) => {
-                  if (item.from == userId) {
-                    return <ChatMe key={index} message={item.body} />;
-                  } else {
-                    return <ChatOther key={index} message={item.body} />;
-                  }
-                })
+             people && messages && messages.length > 0 ? (
+                <>
+                  <button onClick={() => getReadChat(people.friend)}>
+                    load data lama
+                  </button>
+                  {messages.map((item, index) => {
+                    if (item.from == userId) {
+                      return <ChatMe key={index} message={item.body} />;
+                    } else {
+                      return <ChatOther key={index} message={item.body} />;
+                    }
+                  })}
+                </>
               ) : (
                 <div className="text-center my-6 w-full">
                   <h3 className="text-body">
